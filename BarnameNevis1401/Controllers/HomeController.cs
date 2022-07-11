92f38c8f256318c70c877063dc9e0fb932fc4883
ali <ali@gmail.com>
2022-07-11 21:32:03 +0430

    jalse3

--- BarnameNevis1401/Controllers/HomeController.cs
+++ BarnameNevis1401/Controllers/HomeController.cs
@@ -28,4 +28,16 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    [HttpGet]
+    public IActionResult New()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult New(Note note)
+    {
+        return Json(note);
+    }
 }
---
+++ BarnameNevis1401/Models/Note.cs
@@ -0,0 +1,7 @@
+﻿namespace BarnameNevis1401.Models;
+
+public class Note
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
