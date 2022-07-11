e251bfac194f9ecfffa0dea4722b4ebd0df1671f
ali <ali@gmail.com>
2022-07-11 20:29:49 +0430

    init

---
+++ BarnameNevis1401/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BarnameNevis1401.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
